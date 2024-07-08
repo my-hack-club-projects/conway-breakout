5baeaffd90577d04a052413c239edc92d67008db
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-08 18:37:27 +0200

    Pass the game object to all states

--- classes/state.lua
+++ classes/state.lua
@@ -2,10 +2,12 @@ local oo = require 'lib.oo'
 
 local State = oo.class()
 
-function State:init(name)
+function State:init(name, game)
     assert(type(name) == 'string', 'State name must be a string')
+    assert(game, 'State must have a game object')
 
     self.name = name
+    self.game = game
 
     self.enter = function() end
     self.exit = function() end
